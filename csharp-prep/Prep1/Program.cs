b62a3723ace7b66746788f0d9474760ce4023586
Nathan Yochim <neyochim@gmail.com>
2024-04-27 17:02:42 -0600

    Changed the program from printing the Hellow World message to print the James Bond program as requested.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your FIRST name? "); string NameF = Console.ReadLine();
+        Console.WriteLine("What is your LAST name? "); string NameL = Console.ReadLine();
+        Console.WriteLine($"Your name is {NameL}, {NameF} {NameL}.");
     }
 }
