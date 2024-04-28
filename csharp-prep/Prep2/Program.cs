17410476e9d205bf43f23d0a898ffd1e84dccc95
Nathan Yochim <neyochim@gmail.com>
2024-04-27 18:37:46 -0600

    I completed the assignment as instructed and it functions and instructed.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int numberGrade = int.Parse(userInput);
+
+        string letterGrade = "";
+
+        if (numberGrade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}.");
+        if (numberGrade >= 70)
+        {
+            Console.WriteLine("Congradulations! You have a passing grade!");
+        }
+        else
+        {
+            Console.WriteLine("You do not have a passing grade. You did not pass the class. Better luck next time!");
+        }
     }
 }
