6889bc7aa3405b87dc3d7237efc12d9d27dcbbcb
Nathan Yochim <neyochim@gmail.com>
2024-05-02 23:20:17 -0600

    I made the code as requested.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Guess the magic number!");
+        Random randomGenerator = new Random();
+        int magicInt = randomGenerator.Next(1, 11);
+        int guessInt = 0;
+        while (guessInt != magicInt)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessInt = int.Parse(guess);
+            if (guessInt == magicInt)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guessInt < magicInt)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
